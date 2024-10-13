0b5a887bf3fcc04e2d531c4439444f82b83901dc
UrvishSuhagiya <157017938+UrvishSuhagiya@users.noreply.github.com>
2024-10-13 14:36:33 +0530

    Update Day_1_Solution.sh

--- Challenges/DAY_1/Day_1_Solution.sh
+++ Challenges/DAY_1/Day_1_Solution.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# TASK 1 COMMENT
+
+# Task 2 Using echo command to display message
+echo "It's Day 1 Of 7Daysofshellscripting."
+
+#Task 3 Declare variable and assign values
+name="Urvish"
+greet="Hello"
+echo "$greet , $name ! "
+
+# Task 4 Takingg 2 no. as input & print Sum
+num1=10
+num2=20
+sum=$((num1+num2))
+echo "$num1 + $num2 = $sum"
+
+#Task 5 using built in variables
+echo "I AM $whoami ."
+echo "The name of the script is $0"
+echo "Currently I Am At $PWD Location."
+
+#Task 6 Wildcards
+echo "Listing all .sh file in this directory : "
+ls *.sh
+
+# make sure that before run this script run first below command :
+# chmod +x Day_1_Solution.sh
